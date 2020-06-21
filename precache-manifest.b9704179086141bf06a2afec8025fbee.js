ab1631028e0153bd71880a3987147882e3859dd6
aliattallah <alyhesham,alyh@gmail.com>
2020-06-21 23:04:36 +0200

    Updates

---
+++ precache-manifest.b9704179086141bf06a2afec8025fbee.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007dc4876f62998ef89a182a5dfd26e8",
+    "url": "/Aly-Weather/index.html"
+  },
+  {
+    "revision": "1fd57609a8e450394ddc",
+    "url": "/Aly-Weather/static/css/2.494c918c.chunk.css"
+  },
+  {
+    "revision": "503ba17a48a1b55758dd",
+    "url": "/Aly-Weather/static/css/main.75ae9dbb.chunk.css"
+  },
+  {
+    "revision": "1fd57609a8e450394ddc",
+    "url": "/Aly-Weather/static/js/2.7b8836e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Aly-Weather/static/js/2.7b8836e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503ba17a48a1b55758dd",
+    "url": "/Aly-Weather/static/js/main.80154e73.chunk.js"
+  },
+  {
+    "revision": "7bbc5f2fd2c5cc730ae1",
+    "url": "/Aly-Weather/static/js/runtime-main.25f90982.js"
+  },
+  {
+    "revision": "b60a461dbf51aea9a92c451e6a483b4a",
+    "url": "/Aly-Weather/static/media/pexels-photo-1187974.b60a461d.jpeg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/Aly-Weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/Aly-Weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/Aly-Weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/Aly-Weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/Aly-Weather/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
